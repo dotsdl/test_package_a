33e50168e95b80f2d5de16be6787bff43e287493
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-05-12 14:08:19 -0600

    Add simple entry point loading.

--- test_org/test_package_a/__init__.py
+++ test_org/test_package_a/__init__.py
@@ -4,6 +4,8 @@ The first of two pages to test implicit namespaces
 """
 
 # Add imports here
+import pkg_resources
+
 from .test_package_a import *
 
 # Handle versioneer
@@ -12,3 +14,10 @@ versions = get_versions()
 __version__ = versions['version']
 __git_revision__ = versions['full-revisionid']
 del get_versions, versions
+
+for entry_point in pkg_resources.iter_entry_points(
+    "test_org.test_package_a.plugins"
+):
+
+    entry_point.load()
+    print(entry_point, dir(entry_point))
